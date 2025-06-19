f856ecca49fa0670f48de1d4415f6c730e76e289
Anna Rychkova <aanytkaa@gmail.com>
2025-06-19 22:41:51 +0100

    deploy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,9 @@ const eslintConfig = [
     files: ["src/generated/**"],
     rules: {
       "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
 ];
